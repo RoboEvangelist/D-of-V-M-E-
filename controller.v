040f5ac8c820900f74463d40a89a5a619bdbc194
Luis Bill <elpot_15@hotmail.com>
2016-12-02 20:34:14 -0800

    Improved controller code. Check indentation

--- controller.v
+++ controller.v
@@ -1,48 +1,50 @@
-module control (clock,start,s1s2mux,newdist,compstart,peready,vectorx,vectory,addressR,addresss1,addresss2);
+module control (clock, start, s1s2_mux, new_dist, comp_start,
+   pe_ready, vector_x, vector_y, address_r, address_s1, address_s2);
+
 input clock;
-input start; // = 1 when working
-output [15:0] s1s2mux;
-output [15:0] newdist;
-output compstart;
-output [15:0] peready;
-output [3:0] vectorx, vectory;
-output [7:0] addressR;
-output [9:0] addresss1, addresss2;
-reg [12:0] count; //Internal register counter 13 bit 
-reg [12:0] temp;
-reg [15:0] newdist;
-reg compstart; 
+input start; // = 1 when 'going'
+output [15:0] s1s2_mux;
+output [15:0] new_dist;
+output comp_start;
+output [15:0] pe_ready;
+output [3:0] vector_x, vector_y;
+output [7:0] address_r;
+output [9:0] address_s1, address_s2;
+reg [15:0] s1s2_mux;
+reg [15:0] new_dist;
+reg comp_start;
+reg [15:0] pe_ready;
+reg [3:0] vector_x, vector_y;
+reg [7:0] address_r;
+reg [9:0] address_s1, address_s2;
+reg [11:0] count;
 reg completed;
-reg [7:0] addressR;
-reg [9:0] addresss1, addresss2;
-reg [15:0] s1s2mux;
-reg [3:0] vectorx, vectory;
-reg [15:0] peready;
+reg [11:0] temp;
 integer i;
 
-always@(posedge clock)
-begin
-if (start==0) count = 0;
-else  count = count + 12'b1;
+always @(posedge clock)
+
+if (completed == 0) count <= count + 1'b1;
+else if (start == 0 ) count <= 12'b0;
+
+always @(count)
+   begin
+      for (i = 0; i < 15; i = i + 1)
+         begin
+            // TODO: unroll this loop to increase speed.
+            // Check the hardware footprint and power to see if
+            // unrolling is benefitial at all
+            new_dist[i] = (count[7:0] == i );
+            pe_ready[i] = (new_dist[i] && !(count < 256));
+            s1s2_mux[i] = (count[3:0] >= i);
+            comp_start = (!(count < 256));
+         end
+      temp = count[11:0] - 16;
+      address_r = count[7:0];
+      address_s1 = (count[11:8] + count[7:4]) * 31 + count[3:0];
+      address_s2 = (temp[11:8] + temp[7:4]) * 31 + temp[3:0] + 16;
+      vector_x = count[3:0] - 8;
+      vector_y = count[11:8] - 9;
+      completed = (count[11:0] == (16 * 257) - 1);
 end
-
-always@(count)
-begin
-for (i=0;i<16;i=i+1)
-begin
-newdist[i] = (count[7:0] == i);
-peready[i] = (newdist[i] && !(count<256));
-s1s2mux[i] = (count[3:0]>=i);
-compstart = (!(count<256));
-end          
-addressR = count[7:0];
-addresss1 = (count[11:8] + count[7:4]) *31 + count[3:0] ;
-temp = count[11:0] - 16;  
-addresss2 = (temp[11:8] + temp[7:4])*31 + temp[3:0] + 16;
-vectorx = count[3:0] - 8;
-vectory = count[11:8] - 8;
-completed = (count[12:0]== 16*(256+1));
-end 
-endmodule   
-     
-
+endmodule
